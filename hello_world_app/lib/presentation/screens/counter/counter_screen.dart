04186803fbf793ff5236f73866aa1d5941a75e00
Edwinhdzcm <230425@utxicotepec.edu.mx>
2026-09-08 16:27:55 -0600

    Practica 02
    Primera aplicacion con flutter

---
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        body: Center(child: Text('counter screen')),
+      );
+  }
+}
