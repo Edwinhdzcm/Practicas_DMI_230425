50dcc59279853fcd4cda22c6739c653aac8f8c0e
Edwinhdzcm <230425@utxicotepec.edu.mx>
2026-09-20 18:47:45 -0600

    Practica 03
    Se agrego contenido a la practica en la parte del chat

--- Practica03/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ Practica03/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -11,10 +13,11 @@ class ChatScreen extends StatelessWidget {
         leading: const Padding(
           padding: EdgeInsets.all(4.0),
           child: CircleAvatar(
-            backgroundImage: NetworkImage('https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=150'),
+            backgroundImage: NetworkImage(
+                'https://www.stylist.co.uk/images/app/uploads/2022/06/01105352/jennifer-aniston-crop-1654077521-1390x1390.jpg?w=256&h=256&fit=max&auto=format%2Ccompress'),
           ),
         ),
-        title: const Text('Mi amor ♥️'),
+        title: const Text('Bro '),
         centerTitle: false,
       ),
       body: _ChatView(),
@@ -31,14 +34,16 @@ class _ChatView extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return const MyMessageBubble();
-                },
-              ),
-            ),
-            const Text('Mundo'),
+                child: ListView.builder(
+                    itemCount: 100,
+                    itemBuilder: (context, index) {
+                      return ( index % 2 == 0 )
+                        ? const HerMessageBubble()
+                        : const MyMessageBubble();
+                    })),
+
+            /// Caja de texto de mensajes
+            const MessageFieldBox(),
           ],
         ),
       ),
