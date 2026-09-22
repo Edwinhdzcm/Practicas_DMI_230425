4bb93de3c997c48b5b4f35c79c144d5622010f3e
Edwinhdzcm <230425@utxicotepec.edu.mx>
2026-09-21 18:19:21 -0600

    Practica 03
    Se agrego el domain, infraestructura y providers

--- Practica03/yes_no_app/lib/config/theme/app_theme.dart
+++ Practica03/yes_no_app/lib/config/theme/app_theme.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-const Color _customColor = Color(0xFF6200EE);
+const Color _customColor = Color(0xFF49149F);
 
-const List<Color> colorThemes = [
+const List<Color> _colorThemes = [
   _customColor,
   Colors.blue,
   Colors.teal,
@@ -15,17 +15,15 @@ const List<Color> colorThemes = [
 class AppTheme {
   final int selectedColor;
 
-  AppTheme({
-    this.selectedColor = 0,
-  }) : assert(
-          selectedColor >= 0 && selectedColor < colorThemes.length,
-          'Colors must be between 0 and ${colorThemes.length - 1}',
-        );
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0 && selectedColor <= _colorThemes.length - 1,
+            'Colors must be between 0 and ${_colorThemes.length}');
 
   ThemeData theme() {
     return ThemeData(
-      useMaterial3: true,
-      colorSchemeSeed: colorThemes[selectedColor],
-    );
+        useMaterial3: true, 
+        colorSchemeSeed: _colorThemes[selectedColor],
+        // brightness: Brightness.dark
+      );
   }
 }
